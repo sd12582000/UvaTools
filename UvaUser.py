599027117c9cbef8ef75a1590b03956357e8946f
OldNick <sd12582000@gmail.com>
2017-05-19 09:48:50 +0800

    add UvaUser Submit data provider

---
+++ UvaUser.py
@@ -0,0 +1,102 @@
+"""
+Uva User Statistics Object
+"""
+
+class UvaUser:
+    """
+    Uva User Statistics
+    """
+
+    def __init__(self, user_name):
+        from . import UvaProblems
+        from .lib import uhunt_api
+        from .lib import GeneralMethod
+
+        self.problem_set = UvaProblems()
+        self.uid = uhunt_api.get_user_id(user_name)
+        self.try_set = set()
+        self.ac_set = set()
+
+        package = "{}.{}".format(__package__, "Users")
+        file_name = "{}.json".format(self.uid)
+        #print(package)
+
+        try:
+            self.submit = GeneralMethod.load_json_data(package, file_name)
+            #print("load file")
+        except (FileNotFoundError, AttributeError):
+            self.submit = uhunt_api.get_user_submit(self.uid)
+            self.save_user_sumbit()
+            #print("get from uhunt")
+        finally:
+            self.init_user_statistics()
+
+    def init_user_statistics(self):
+        """
+        init User Sumbit Statistics by user_submit
+        Data Struct:
+
+        Submission ID
+        Problem ID
+        Verdict ID
+        Runtime
+        Submission Time (unix timestamp)
+        Language ID (1=ANSI C, 2=Java, 3=C++, 4=Pascal, 5=C++11)
+        Submission Rank
+
+        Verdict ID can be one of the following values:
+
+        10 : Submission error
+        15 : Can't be judged
+        20 : In queue
+        30 : Compile error
+        35 : Restricted function
+        40 : Runtime error
+        45 : Output limit
+        50 : Time limit
+        60 : Memory limit
+        70 : Wrong answer
+        80 : PresentationE
+        90 : Accepted
+
+        Args:
+            self: self
+
+        Returns:
+            None
+        """
+        for sub in self.submit:
+            pro_num = self.problem_set.problem_id_to_num(sub[1])
+            self.try_set.add(pro_num)
+            if sub[2] == 90:
+                self.ac_set.add(pro_num)
+
+    def save_user_sumbit(self):
+        """
+        save user sumbit data to json file
+
+        Args:
+            self: self
+
+        Returns:
+            None
+        """
+        from .lib import GeneralMethod
+        from os.path import abspath, join, dirname
+        file_path = join(join(abspath(dirname(__file__)), "Users"), "{}.json".format(self.uid))
+        GeneralMethod.save_json_data(self.submit, file_path)
+
+    def is_acept(self, problem_num):
+        """
+        Is problem solved
+
+        Args:
+            self: self
+            problem_num: Uva Problem Num
+
+        Returns:
+            boolen
+            Is Problem solve?
+        """
+
+        return problem_num in self.ac_set
